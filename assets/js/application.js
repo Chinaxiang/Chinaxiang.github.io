66c9aed39aa4e20ef20ccf79e5adce50e78167dd
chinaxiang <yanxiang.huang@gnetoffice.com>
2017-09-22 18:06:26 +0800

    custom

---
+++ assets/js/application.js
@@ -0,0 +1,93 @@
+$(document).on("focus", '[data-action="grow"]', function() {
+  $(window).width() > 1e3 && $(this).animate({
+    width: 300
+  })
+}),
+$(document).on("blur", '[data-action="grow"]', function() {
+  if ($(window).width() > 1e3) {
+    $(this).animate({
+      width: 180
+    })
+  }
+}),
+// 滚动到页面顶部
+$(function() {
+  var scrollTop = $('<a href="javascript:;" class="scroll-top" style="display: none;position: fixed;bottom: 10px;right: 10px;z-index: 999;"><img src="https://o0y5cx8y8.qnssl.com/top.png"></a>');
+  $('body').append(scrollTop);
+  function o() {
+    $(window).scrollTop() > $(window).height() ? $(".scroll-top").fadeIn() : $(".scroll-top").fadeOut()
+  }
+  $(".scroll-top").length && (o(), $(window).on("scroll", o));
+  $("body").on("click", ".scroll-top", function(e){
+    e.preventDefault();
+    $('body,html').animate({scrollTop: 0}, 500);
+  });
+});
+$(function(){
+  // 查看个人简历时的口令输入
+  var lock = true;
+  $("body").on("click", ".unlock", function(){
+    if (lock) {
+      var pwd = prompt("请输入口令","");
+      if (pwd) {
+        alert("您输入的口令错误。");
+      }
+    }
+  });
+});
+(function(){ 
+var SOHUCS = $('#SOHUCS').get(0);
+if (!SOHUCS) {
+  return;
+}
+var appid = 'cytea86wO';
+var conf = 'prod_44a893a22d93436e9c1bb8138f5ee308';
+var width = window.innerWidth || document.documentElement.clientWidth;
+if (width < 960) {
+  window.document.write('<script id="changyan_mobile_js" charset="utf-8" type="text/javascript" src="http://changyan.sohu.com/upload/mobile/wap-js/changyan_mobile.js?client_id=' + appid + '&conf=' + conf + '"><\/script>');
+} else {
+  var loadJs = function(d, a) {
+    var c = document.getElementsByTagName("head")[0] || document.head || document.documentElement;
+    var b = document.createElement("script");
+    b.setAttribute("type", "text/javascript");
+    b.setAttribute("charset", "UTF-8");
+    b.setAttribute("src", d);
+    if (typeof a === "function") {
+      if (window.attachEvent) {
+        b.onreadystatechange = function() {
+          var e = b.readyState;
+          if (e === "loaded" || e === "complete") {
+            b.onreadystatechange = null;
+            a()
+          }
+        }
+      } else {
+        b.onload = a
+      }
+    }
+    c.appendChild(b)
+  };
+  loadJs("http://changyan.sohu.com/upload/changyan.js",
+  function() {
+    window.changyan.api.config({
+      appid: appid,
+      conf: conf
+    })
+  });
+}
+})();
+/*placeholder 图片错误处理*/
+function imgerror(e) {
+  var w = $(e).data("width");
+  var h = $(e).data("height");
+  var name = $(e).data("name");
+  var opts = {
+    size: w + 'x' + h,
+    bgcolor: '#ccc', 
+    color: '#ffffff',
+    text: name + ' 404 Not Found',
+    fsize:'14',
+    ffamily: 'consolas'
+  }
+  e.src = placeholder.getData(opts);
+}
---
+++ assets/js/blog.js
@@ -0,0 +1,85 @@
+$(function(){
+  w = function() {
+      var e, t, n, r, i, o, a;
+      e = $(".article"),
+      t = e.find("h2, h3, h4"),
+      $(".side").find(".post-nav").addClass("side-outline").data("width", $(".side-outline").width()).data("top", $(".side-outline").offset().top),
+      r = $(".side-outline"),
+      n = r.find(".highlight-title"),
+      o = r.offset().top,
+      $(".nav-body").css("top", 0),
+      $(window).scroll(function() {
+          var e, i, o, a;
+          i = $(this).scrollTop(),
+          a = void 0,
+          e = void 0,
+          o = void 0,
+          i > r.data("top") + 20 ? i + r.height() + parseFloat($(".nav-body").css("top").replace(/px/, "")) < $("footer").offset().top ? r.removeClass("absolute").addClass("fixed").width(r.data("width")).css("top", 0) : r.removeClass("fixed").addClass("absolute").css("top", r.parents(".row").height() - r.height()) : r.removeClass("fixed").css("width", "100%"),
+          t.eq(0).offset().top < i ? (n.show(), t.each(function(s, l) {
+              return e = r.find("[href=#" + $(this).attr("id") + "]"),
+              o = t.last().offset().top - i,
+              t.eq(Math.min(Math.max(0, s + 1), t.length - 1)).offset().top > i + 20 || 20 > o ? (20 > o && (e = r.find("a").last()), e.offset() && (a = e.offset().top - e.parents(".nav-body").offset().top), e.parents(".articleIndex").find("li").removeClass("active"), e.parent().addClass("active"), n.css("top", a).height(e.outerHeight()), a > $(window).height() / 2 ? $(".nav-body").css("top", -(e.parent().offset().top - e.parents(".nav-body").offset().top - $(window).height() / 2)) : $(".nav-body").css("top", 0), !1) : void 0
+          })) : ($(".articleIndex").find("li").removeClass("active"), n.hide());
+      }),
+      a = 1,
+      i = $(".articleIndex"),
+      t.each(function(e) {
+          var t, n, r;
+          r = $(this).text().trim(),
+          "" !== r && ($(this).attr("id", "articleHeader" + e), n = parseInt($(this)[0].tagName.slice(1)), t = null, 0 === e || n === a ? (t = $('<li><a href="#articleHeader' + e + '"></a></li>'), t.find("a").text(r), i.append(t)) : n > a ? (t = $("<li style='list-style:none;'><ul><li><a href=\"#articleHeader" + e + '"></a></li></ul></li>'), t.find("a").text(r), i.append(t), i = t.find("ul")) : a > n && (1 === n ? (t = $('<li><a href="#articleHeader' + e + '"></a></li>'), t.find("a").text(r), $(".articleIndex").append(t), i = $(".articleIndex")) : (t = $("<li style='list-style:none;'><ul><li><a href=\"#articleHeader" + e + '"></a></li></ul></li>'), t.find("a").text(r), i.parents("ul").parents("ul").length ? (i.parents("ul").parents("ul").append(t), i = t.find("ul")) : (t = $('<li><a href="#articleHeader' + e + '"></a></li>'), t.find("a").text(r), $(".articleIndex").append(t), i = $(".articleIndex")))), a = n)
+      });
+    };
+
+  $(".article").find("h2, h3, h4").length && ($(".post-nav").show(), w(), 0 === $(".articleIndex li").length && $(".widget-outline").remove());
+  // 对文章进行赞操作
+  var lock = false;
+  $("body").on("click", "#sideLike", function(){
+    var ele = $(this);
+    var id = ele.data('id');
+    if (!lock) {
+      lock = true;
+      var numEle = ele.parent().find("#sideLiked");
+      var num = Number(numEle.text()) + 1;
+      numEle.text(num);
+    }
+  });
+});
+
+$(function() {
+  "use strict";
+  //$("pre").find("code").parent().addClass('highlight');
+  $(".article img").addClass('img-responsive');
+  var b = $(window),c = $(document.body);
+  ZeroClipboard.config({
+    moviePath: "//cdn.bootcss.com/zeroclipboard/2.1.6/ZeroClipboard.swf",
+    hoverClass: "btn-clipboard-hover"
+  });
+  $(".highlight").each(function() {
+    var b = '<div class="zero-clipboard hidden-sm hidden-xs"><span class="btn-clipboard">复制</span></div>';
+    $(this).before(b);
+  });
+ 
+  var d = new ZeroClipboard($(".btn-clipboard"));
+  var e = $("#global-zeroclipboard-html-bridge");
+  d.on("ready",function() {
+    e.data("placement", "top").attr("title", "复制到剪贴板").tooltip(),
+    d.on('copy',function(event) {
+      var c=$(event.target).parent().nextAll(".highlight").first();
+        event.clipboardData.setData('text/plain', c.text());
+    });
+    d.on("aftercopy",function() {
+        e.attr("title", "复制成功！").tooltip("fixTitle").tooltip("show").attr("title", "复制到剪贴板").tooltip("fixTitle")
+    });
+  });
+  d.on("noflash wrongflash",function() {
+    $(".zero-clipboard").remove();
+    ZeroClipboard.destroy();
+  });
+  $(window).load(function(){
+    $(".highlight").mCustomScrollbar({
+      theme: "dark"
+    });
+  });
+  /*hljs.initHighlightingOnLoad();*/
+});
+
